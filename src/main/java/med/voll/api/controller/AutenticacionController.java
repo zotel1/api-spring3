1e889eff7d9c662123da0d52b61b8e3a36b604bb
zotel1 <zotelsigel@gmail.com>
2024-06-14 22:57:24 -0300

    update

---
+++ src/main/java/med/voll/api/controller/AutenticacionController.java
@@ -0,0 +1,27 @@
+package med.voll.api.controller;
+
+
+import med.voll.api.domain.usuarios.DatosAutenticacionUsuario;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacionController {
+
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public ResponseEntity autenticarUsuario(DatosAutenticacionUsuario datosAutenticacionUsuario) {
+
+        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUsuario.clave(), datosAutenticacionUsuario.login());
+        authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/med/voll/api/domain/usuarios/Usuario.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.usuarios;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Usuario {
+
+    private Long id;
+    private String login;
+    private String clave;
+}
