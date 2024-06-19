3371d80086057557a0adcac52453b880e79ca160
zotel1 <zotelsigel@gmail.com>
2024-06-19 17:12:42 -0300

    update jwt token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,6 +12,10 @@ import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private  TokenService tokenService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // Obtener el token del header
@@ -20,6 +25,7 @@ public class SecurityFilter extends OncePerRequestFilter {
         }
         token = token.replace("Bearer ", "");
         System.out.println(token);
+        System.out.println(tokenService.getSubject(token)); // Este usuario tiene sesion?
 
         filterChain.doFilter(request, response);
     }
