1225d559fe6be2575f530237434f48b02f0ff457
zotel1 <zotelsigel@gmail.com>
2024-08-01 22:49:58 -0300

    creamos una nueva clase de validación llamada PacienteActivo

--- src/main/java/med/voll/api/domain/consulta/validaciones/PacienteActivo.java
+++ src/main/java/med/voll/api/domain/consulta/validaciones/PacienteActivo.java
@@ -1,6 +1,8 @@
 package med.voll.api.domain.consulta.validaciones;
 
+import jakarta.validation.ValidationException;
 import med.voll.api.domain.consulta.DatosAgendarConsulta;
+import med.voll.api.domain.paciente.PacienteRepository;
 
 public class PacienteActivo {
 
@@ -11,6 +13,10 @@ public class PacienteActivo {
             return;
         }
 
+        var pacienteActivo = repository.findActivoById(datos.idPaciente());
 
+        if (!pacienteActivo) {
+            throw new ValidationException("No se pueden permitir agendar citas con pacientes inactivos en el sistema");
+        }
     }
 }
---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.paciente;
+
+public class Paciente {
+}
