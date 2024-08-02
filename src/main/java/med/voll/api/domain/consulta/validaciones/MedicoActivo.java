e47e70a9c605843c21e0be46607b583f20cccbc6
zotel1 <zotelsigel@gmail.com>
2024-08-01 23:19:48 -0300

    creamos una nueva clase de validación llamada MedicoActivo

---
+++ src/main/java/med/voll/api/domain/consulta/validaciones/MedicoActivo.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta.validaciones;
+
+import jakarta.validation.ValidationException;
+import med.voll.api.domain.consulta.DatosAgendarConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+
+public class MedicoActivo {
+
+    private MedicoRepository repository;
+
+    public void validar(DatosAgendarConsulta datos) {
+        if(datos.idMedico()==null) {
+            return;
+        }
+        var medicoActivo = repository.findActivoById(datos.idMedico());
+
+        if (!medicoActivo) {
+            throw new ValidationException("No se pueden permitir citas con un medico inactivo");
+        }
+    }
+}
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -23,4 +23,6 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             limit 1
             """)
     Medico seleccionarMedicoConEspecialidadEnFecha(Especialidad especialidad, LocalDateTime fecha);
+
+    Boolean findActivoById(Long aLong);
 }
