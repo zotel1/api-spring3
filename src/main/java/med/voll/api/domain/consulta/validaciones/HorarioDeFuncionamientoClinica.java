22bcc4291ce38085f1de9b1c12b52954f9630811
zotel1 <zotelsigel@gmail.com>
2024-08-25 23:31:04 -0300

    update: refactorizando todo

--- src/main/java/med/voll/api/domain/consulta/validaciones/HorarioDeFuncionamientoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validaciones/HorarioDeFuncionamientoClinica.java
@@ -7,17 +7,15 @@ import org.springframework.stereotype.Component;
 import java.time.DayOfWeek;
 
 @Component
-public class HorarioDeFuncionamientoClinica implements ValidadorDeConsultas {
-    public void validad(DatosAgendarConsulta datos) {
+public class HorarioDeFuncionamientoClinica implements ValidadorDeConsultas{
+    public void validar(DatosAgendarConsulta datos) {
 
         var domingo = DayOfWeek.SUNDAY.equals(datos.fecha().getDayOfWeek());
 
-        var antesDeApertura = datos.fecha().getHour() < 7;
-
-        var despuesDeCierre = datos.fecha().getHour() > 19;
-
-        if (domingo || antesDeApertura || despuesDeCierre) {
-            throw new ValidationException("El horario de atención de la clínica es de lunes a sábado, de 07:00 a 19:00 horas.");
+        var antesdDeApertura=datos.fecha().getHour()<7;
+        var despuesDeCierre=datos.fecha().getHour()>19;
+        if(domingo || antesdDeApertura || despuesDeCierre){
+            throw  new ValidationException("El horario de atención de la clínica es de lunes a sábado, de 07:00 a 19:00 horas");
         }
     }
 }
