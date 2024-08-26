8fdbdf52ca2643e79f7bb4a4787335095580ec88
zotel1 <zotelsigel@gmail.com>
2024-08-25 23:36:00 -0300

    update: refactorizando todo

--- src/main/java/med/voll/api/domain/consulta/desafio/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/desafio/ValidadorHorarioAntecedencia.java
@@ -1,16 +1,16 @@
 package med.voll.api.domain.consulta.desafio;
 
 import jakarta.validation.ValidationException;
-import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DatosCancelamientoConsulta;
+import med.voll.api.domain.consulta.ConsultaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-@Component
-public class ValidadorHorarioAntecedencia implements ValidadorCancelamientoDeConsulta{
+@Component("ValidadorHorarioAntecedenciaCancelamento")
+public class ValidadorHorarioAntecedencia implements ValidadorCancelamientoDeConsulta {
 
     @Autowired
     private ConsultaRepository repository;
@@ -19,10 +19,10 @@ public class ValidadorHorarioAntecedencia implements ValidadorCancelamientoDeCon
     public void validar(DatosCancelamientoConsulta datos) {
         var consulta = repository.getReferenceById(datos.idConsulta());
         var ahora = LocalDateTime.now();
-        var diferenciaEnHoras = Duration.between(ahora, consulta.getData().toHours());
+        var diferenciaEnHoras = Duration.between(ahora, consulta.getFecha()).toHours();
 
         if (diferenciaEnHoras < 24) {
-            throw new ValidationException("Consulta solamente puede ser cancelada media hora antes");
+            throw new ValidationException("Consulta solamente puede ser cancelada con antecedencia mínima de 24h!");
         }
     }
 }
